8328d9c7d7535e6bc0e21fe15344b36befe0f3f1
ridhimagarg <garg.ridhima72@gmail.com>
2022-07-29 23:11:23 +0200

    changed get data

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -2,8 +2,8 @@ import os
 from os import path
 import shutil
 
-src = "../../data/domain_pics/train/"
-dst = "../../data/final_train/real/"
+src = "../data/domain_pics/train/"
+dst = "../data/final_train/real/"
 print("Data moved from real train to final_train real..")
 
 cnt = 0
@@ -12,8 +12,8 @@ files = [i for i in os.listdir(src)]
 for f in files:
         shutil.copy(path.join(src, f), dst)
 
-src = "../../data/domain_pics/test/"
-dst = "../../data/final_test/real/"
+src = "../data/domain_pics/test/"
+dst = "../data/final_test/real/"
 print("Data moved from real test to final_test real..")    
     
 cnt = 0
@@ -22,8 +22,8 @@ files = [i for i in os.listdir(src)]
 for f in files:
         shutil.copy(path.join(src, f), dst)
         
-src = "../../data/fake_pics/train/"
-dst = "../../data/final_train/phish/"
+src = "../data/fake_pics/train/"
+dst = "../data/final_train/phish/"
 print("Data moved from fake_train to final_train fake..")        
 
 cnt = 0
@@ -32,8 +32,8 @@ files = [i for i in os.listdir(src)]
 for f in files:
         shutil.copy(path.join(src, f), dst)
         
-src = "../../data/fake_pics/test/"
-dst = "../../data/final_test/phish/"
+src = "../data/fake_pics/test/"
+dst = "../data/final_test/phish/"
 print("Data moved from fake test to final test fake..")        
 
 cnt = 0
@@ -42,8 +42,8 @@ files = [i for i in os.listdir(src)]
 for f in files:
         shutil.copy(path.join(src, f), dst)
         
-src = "../../data/domain_pics/valid/"
-dst = "../../data/final_valid/real/"
+src = "../data/domain_pics/valid/"
+dst = "../data/final_valid/real/"
 print("Data moved from real valid to final valid real..")        
 cnt = 0
 #files = [i for i in os.listdir(src) if i.startswith("CTASK") and path.isfile(path.join(src, i))]
@@ -51,8 +51,8 @@ files = [i for i in os.listdir(src)]
 for f in files:
         shutil.copy(path.join(src, f), dst)
         
-src = "../../data/fake_pics/valid/"
-dst = "../../data/final_valid/phish/"
+src = "../data/fake_pics/valid/"
+dst = "../data/final_valid/phish/"
 print("Data moved from fake valid to final_valid fake..")        
 
 cnt = 0
--- src/get_data.py
+++ src/get_data.py
@@ -6,7 +6,7 @@ import pickle
 import os
 
 data = pickle.load(urllib.request.urlopen('https://github.com/endgameinc/homoglyph/blob/master/data/domains_spoof.pkl?raw=true'))
-font = 'ARIAL.TTF'
+font = '../data/ARIAL.TTF'
 # BASE_DIR = ''
 
 def img(text, path):     
@@ -29,99 +29,99 @@ uniq_valid = list(set(valid))
 
 print(len(uniq_train), len(uniq_test), len(uniq_valid))
 
-with open("../../data/domains_train.txt", "wb") as fp:
+with open("../data/domains_train.txt", "wb") as fp:
     pickle.dump(uniq_train, fp)
 
-with open("../../data/domains_test.txt", "wb") as fp:
+with open("../data/domains_test.txt", "wb") as fp:
     pickle.dump(uniq_test, fp)
 
-with open("../../data/domains_valid.txt", "wb") as fp:
+with open("../data/domains_valid.txt", "wb") as fp:
     pickle.dump(uniq_valid, fp)
     
-with open("../../data/domains_train.txt", "rb") as fp:
+with open("../data/domains_train.txt", "rb") as fp:
     domains_train = pickle.load(fp)
 
-with open("../../data/domains_test.txt", "rb") as fp:
+with open("../data/domains_test.txt", "rb") as fp:
     domains_test = pickle.load(fp)
     
-with open("../../data/domains_valid.txt", "rb") as fp:
+with open("../data/domains_valid.txt", "rb") as fp:
     domains_valid = pickle.load(fp)
     
 print(len(domains_train), len(domains_valid), len(domains_test))
 
 print("Creating Directories for data schema..")
 
-os.mkdir('../../data/domain_pics')
-os.mkdir('../../data/domain_pics/train')
-os.mkdir('../../data/domain_pics/test')
-os.mkdir('../../data/domain_pics/valid')
+# os.mkdir('../data/domain_pics')
+# os.mkdir('../data/domain_pics/train')
+# os.mkdir('../data/domain_pics/test')
+# os.mkdir('../data/domain_pics/valid')
 
 print("Directories for data schema created..")
 
 print("Creating images for real train domains...")
 for domain in domains_train:
-    img(domain, '../../data/domain_pics/train/')
+    img(domain, '../data/domain_pics/train/')
 
 print("Creating images for real test domains...")
 for domain in domains_test:
-    img(domain, '../../data/domain_pics/test/')
+    img(domain, '../data/domain_pics/test/')
 
 print("Creating images for real valid domains...")    
 for domain in domains_valid:
-    img(domain, '../../data/domain_pics/valid/')
+    img(domain, '../data/domain_pics/valid/')
     
 train = [dom[1] for dom in data['train']][:69723]
 test = [dom[1] for dom in data['test']][:18349]
 valid = [dom[1] for dom in data['validate']][:3670]
 
-with open("../../data/fake_train.txt", "wb") as fp:
+with open("../data/fake_train.txt", "wb") as fp:
     pickle.dump(train, fp)
     
-with open("../../data/fake_test.txt", "wb") as fp:
+with open("../data/fake_test.txt", "wb") as fp:
     pickle.dump(test, fp)
     
-with open("../../data/fake_valid.txt", "wb") as fp:
+with open("../data/fake_valid.txt", "wb") as fp:
     pickle.dump(valid, fp)
     
-with open("../../data/fake_train.txt", "rb") as fp:
+with open("../data/fake_train.txt", "rb") as fp:
     fake_train = pickle.load(fp)
     
-with open("../../data/fake_test.txt", "rb") as fp:
+with open("../data/fake_test.txt", "rb") as fp:
     fake_test = pickle.load(fp)
     
-with open("../../data/fake_valid.txt", "rb") as fp:
+with open("../data/fake_valid.txt", "rb") as fp:
     fake_valid = pickle.load(fp)
 
 print("Creating directories for fake domains")    
-os.mkdir('../../data/fake_pics')
-os.mkdir('../../data/fake_pics/train')
-os.mkdir('../../data/fake_pics/test')
-os.mkdir('../../data/fake_pics/valid')
+os.mkdir('../data/fake_pics')
+os.mkdir('../data/fake_pics/train')
+os.mkdir('../data/fake_pics/test')
+os.mkdir('../data/fake_pics/valid')
 
 print("Creating images for fake train domains...")
 
 for fake in fake_train:
-    img(fake,'../../data/fake_pics/train/')
+    img(fake,'../data/fake_pics/train/')
 
 print("Creating images for fake test domains...")    
 for fake in fake_test:
-    img(fake,'../../data/fake_pics/test/')
+    img(fake,'../data/fake_pics/test/')
 
 print("Creating images for fake valid domains...")    
 for fake in fake_valid:
-    img(fake,'../../data/fake_pics/valid/')
+    img(fake,'../data/fake_pics/valid/')
     
-os.mkdir('../../data/final_train')
-os.mkdir('../../data/final_train/real')
-os.mkdir('../../data/final_train/phish')
+os.mkdir('../data/final_train')
+os.mkdir('../data/final_train/real')
+os.mkdir('../data/final_train/phish')
 
-os.mkdir('../../data/final_valid')
-os.mkdir('../../data/final_valid/real')
-os.mkdir('../../data/final_valid/phish')
+os.mkdir('../data/final_valid')
+os.mkdir('../data/final_valid/real')
+os.mkdir('../data/final_valid/phish')
 
-os.mkdir('../../data/final_test')
-os.mkdir('../../data/final_test/real')
-os.mkdir('../../data/final_test/phish')
+os.mkdir('../data/final_test')
+os.mkdir('../data/final_test/real')
+os.mkdir('../data/final_test/phish')
 
 print("Get data completed..")
                         
